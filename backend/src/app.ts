6e39350f484c0d18d88f969406858a0f3aa05d6f
iltan987 <22109906@student.ciu.edu.tr>
2024-07-04 09:18:54 +0300

    updated backend to not to block connections using cors and disabled recreating tables each time, instead they will remain same

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/index';
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json({limit: '5mb'}));
+
+app.use(cors())
 
 app.use('/api', router);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import sequelize from './config/db';
 const port = process.env.PORT || 3000;
 
 (async () => {
-  await sequelize.sync({force: true});
+  await sequelize.sync({force: false});
 
   app.listen(port, () => {
     console.info(`Server running on port ${port}`);
